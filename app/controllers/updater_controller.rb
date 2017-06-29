2c8729e47f62bdafb8303104ad241df0098330f7
franco viterbo <francoviterbo@gmail.com>
2017-06-29 00:54:11 -0500

    Script: Donwload file xml.gz

--- app/controllers/updater_controller.rb
+++ app/controllers/updater_controller.rb
@@ -1,4 +1,12 @@
 class UpdaterController < ApplicationController
   def index
+    require 'zlib'
+    require 'open-uri'
+    
+    # Download file xml.gz
+    download = open('http://www.stagingeb.com/feeds/d420256874ddb9b6ee5502b9d54e773d8316a695/trovit_MX.xml.gz')
+    IO.copy_stream(download, '/tmp/trovit_MX.xml.gz')
+
+    @result = "Data updated"
   end
 end
