ad4288da9f214603f933ae4eb2aba4e7bd77e7a5
franco viterbo <francoviterbo@gmail.com>
2017-06-30 18:00:21 -0500

    Location resource nested in Item

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       get 'list'
     end 
     resources :pictures
+    resources :locations
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
