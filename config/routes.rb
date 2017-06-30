0bfd6bbc48ca52d831fe98b12c19211d1d0e0ede
franco viterbo <francoviterbo@gmail.com>
2017-06-30 12:30:27 -0500

    Pictures resource nested in Item resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  
   get 'updater/index'
 
   resources :items do
     collection do
       get 'list'
     end 
+    resources :pictures
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
