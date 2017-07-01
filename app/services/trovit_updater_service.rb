6860fd7d9164f3ee0adb872aaf3a9806c28fae37
franco viterbo <francoviterbo@gmail.com>
2017-06-30 21:56:48 -0500

    build_item_by_trovit_property method extracted

--- app/services/trovit_updater_service.rb
+++ app/services/trovit_updater_service.rb
@@ -29,6 +29,23 @@ class TrovitUpdaterService < UpdaterService
         trovitXmlFile.xpath("//ad").map
     end
 
+    def build_item_by_trovit_property(trovitPropery)
+        itemCreated = Item.create(
+            item_id:trovitPropery.at_css("id").nil? ? "" : trovitPropery.at_css("id").text, 
+            property_type:trovitPropery.at_css("property_type").nil? ? "" : trovitPropery.at_css("property_type").text,
+            url:trovitPropery.at_css("url").nil? ? "" : trovitPropery.at_css("url").text,
+            title:trovitPropery.at_css("title").nil? ? "" : trovitPropery.at_css("title").text,
+            content:trovitPropery.at_css("content").nil? ? "" : trovitPropery.at_css("content").text,
+            type:trovitPropery.at_css("type").nil? ? "" : trovitPropery.at_css("type").text,
+            agency:trovitPropery.at_css("agency").nil? ? "" : trovitPropery.at_css("agency").text,
+            currency:trovitPropery.at_css("price").nil? ? "" : trovitPropery.at_css("price").attr("currency"),
+            price:trovitPropery.at_css("price").nil? ? "" : trovitPropery.at_css("price").text,
+            period:trovitPropery.at_css("price").nil? ? "" : trovitPropery.at_css("price").attr("period"),
+            date:trovitPropery.at_css("date").nil? ? "" : trovitPropery.at_css("date").text,
+            time:trovitPropery.at_css("time").nil? ? "" : trovitPropery.at_css("time").text
+        )
+    end
+
     def update_data
         
         trovitXmlFile = open_trovit_file
@@ -37,67 +54,54 @@ class TrovitUpdaterService < UpdaterService
 
         Item.destroy_all
 
-        trovitProperties.each { |node| 
-        itemCreated = Item.create(
-            item_id:node.at_css("id").nil? ? "" : node.at_css("id").text, 
-            property_type:node.at_css("property_type").nil? ? "" : node.at_css("property_type").text,
-            url:node.at_css("url").nil? ? "" : node.at_css("url").text,
-            title:node.at_css("title").nil? ? "" : node.at_css("title").text,
-            content:node.at_css("content").nil? ? "" : node.at_css("content").text,
-            type:node.at_css("type").nil? ? "" : node.at_css("type").text,
-            agency:node.at_css("agency").nil? ? "" : node.at_css("agency").text,
-            currency:node.at_css("price").nil? ? "" : node.at_css("price").attr("currency"),
-            price:node.at_css("price").nil? ? "" : node.at_css("price").text,
-            period:node.at_css("price").nil? ? "" : node.at_css("price").attr("period"),
-            date:node.at_css("date").nil? ? "" : node.at_css("date").text,
-            time:node.at_css("time").nil? ? "" : node.at_css("time").text
-        )
+        trovitProperties.each { |trovitPropery| 
+        itemCreated = build_item_by_trovit_property(trovitPropery)
 
         # Location management
         itemCreated.create_location(
-            city:node.at_css("city").nil? ? "" : node.at_css("city").text,
-            city_area:node.at_css("city_area").nil? ? "" : node.at_css("city_area").text,
-            region:node.at_css("region").nil? ? "" : node.at_css("region").text,
-            postalcode:node.at_css("postalcode").nil? ? "" : node.at_css("postalcode").text,
-            longitude:node.at_css("longitude").nil? ? "" : node.at_css("longitude").text,
-            latitude:node.at_css("latitude").nil? ? "" : node.at_css("latitude").text
+            city:trovitPropery.at_css("city").nil? ? "" : trovitPropery.at_css("city").text,
+            city_area:trovitPropery.at_css("city_area").nil? ? "" : trovitPropery.at_css("city_area").text,
+            region:trovitPropery.at_css("region").nil? ? "" : trovitPropery.at_css("region").text,
+            postalcode:trovitPropery.at_css("postalcode").nil? ? "" : trovitPropery.at_css("postalcode").text,
+            longitude:trovitPropery.at_css("longitude").nil? ? "" : trovitPropery.at_css("longitude").text,
+            latitude:trovitPropery.at_css("latitude").nil? ? "" : trovitPropery.at_css("latitude").text
         )
 
         # Attributes management
         attributeList = []
-        if (!node.at_css("rooms").nil?)
-            attributeList << {type:"integer",name:"rooms",value:node.at_css("rooms").text,decorated:""}
+        if (!trovitPropery.at_css("rooms").nil?)
+            attributeList << {type:"integer",name:"rooms",value:trovitPropery.at_css("rooms").text,decorated:""}
         end
 
-        if (!node.at_css("bathrooms").nil?)
-            attributeList << {type:"integer",name:"bathrooms",value:node.at_css("bathrooms").text,decorated:""}
+        if (!trovitPropery.at_css("bathrooms").nil?)
+            attributeList << {type:"integer",name:"bathrooms",value:trovitPropery.at_css("bathrooms").text,decorated:""}
         end
 
-        if (!node.at_css("parking").nil?)
-            attributeList << {type:"integer",name:"parking",value:node.at_css("parking").text,decorated:""}
+        if (!trovitPropery.at_css("parking").nil?)
+            attributeList << {type:"integer",name:"parking",value:trovitPropery.at_css("parking").text,decorated:""}
         end
 
-        if (!node.at_css("year").nil?)
-            attributeList << {type:"integer",name:"year",value:node.at_css("year").text,decorated:""}
+        if (!trovitPropery.at_css("year").nil?)
+            attributeList << {type:"integer",name:"year",value:trovitPropery.at_css("year").text,decorated:""}
         end
 
-        if (!node.at_css("is_new").nil?)
-            attributeList << {type:"boolean",name:"is_new",value:node.at_css("is_new").text,decorated:""}
+        if (!trovitPropery.at_css("is_new").nil?)
+            attributeList << {type:"boolean",name:"is_new",value:trovitPropery.at_css("is_new").text,decorated:""}
         end
 
-        if (!node.at_css("floor_area").nil?)
-            attributeList << {type:"number_unit",name:"floor_area",value:node.at_css("floor_area").text,decorated:node.at_css("floor_area").attr("unit")}
+        if (!trovitPropery.at_css("floor_area").nil?)
+            attributeList << {type:"number_unit",name:"floor_area",value:trovitPropery.at_css("floor_area").text,decorated:trovitPropery.at_css("floor_area").attr("unit")}
         end
 
-        if (!node.at_css("plot_area").nil?)
-            attributeList << {type:"number_unit",name:"plot_area",value:node.at_css("plot_area").text,decorated:node.at_css("plot_area").attr("unit")}
+        if (!trovitPropery.at_css("plot_area").nil?)
+            attributeList << {type:"number_unit",name:"plot_area",value:trovitPropery.at_css("plot_area").text,decorated:trovitPropery.at_css("plot_area").attr("unit")}
         end
 
         itemCreated.attribute.create(attributeList)
 
         
         # Picture management
-        pictures = node.at_css("pictures").nil? ? [] : node.at_css("pictures").xpath("picture").map
+        pictures = trovitPropery.at_css("pictures").nil? ? [] : trovitPropery.at_css("pictures").xpath("picture").map
         picturesList = []
         pictures.each{ |picture|
             picturesList << {picture_url:picture.at_css("picture_url").text}
