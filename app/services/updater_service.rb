0bcd2887ae9af9a0ef77a8ccd24f0bc25b9e0ff6
franco viterbo <francoviterbo@gmail.com>
2017-07-02 22:46:25 -0500

    exception messages fixed

--- app/services/updater_service.rb
+++ app/services/updater_service.rb
@@ -1,9 +1,9 @@
 class UpdaterService
     def get_datasource
-        raise 'must implement draw() method in subclass'
+        raise 'must implement get_datasource() method in subclass'
     end
 
     def update_data
-        raise 'must implement draw() method in subclass'
+        raise 'must implement update_data() method in subclass'
     end
 end
